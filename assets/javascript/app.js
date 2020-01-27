74000722972ff4ba26334cec881939d0d707a2cb
Brian Allen <brianallen2401@gmail.com>
2020-01-27 12:27:26 -0700

    added imgArray to help me iterate through images

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -68,7 +68,9 @@ $(document).ready(function () {
             correct: "The Cabinet of Dr. Caligari"
         },
     };
-
+    //array to help me iterate and insert images
+    var imgArray = ["question1", "question2", "question3", "question4", "question5", "question6", "question7", "question8", "question9", "question10", "question11", "question12", "question13"];
+    
     var win = 0;
     var lose = 0;
     var unanswered = 0;
