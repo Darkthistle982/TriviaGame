6842b6a62816f21af2744250dfbab34e75b04c28
Brian Allen <brianallen2401@gmail.com>
2020-01-27 12:41:51 -0700

    added currentquestion variable to help iterate through the questions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -70,6 +70,7 @@ $(document).ready(function () {
     };
     //array to help me iterate and insert images
     var imgArray = ["question1", "question2", "question3", "question4", "question5", "question6", "question7", "question8", "question9", "question10", "question11", "question12", "question13"];
+    var currentQuestion = 0;
     
     var win = 0;
     var lose = 0;
@@ -128,6 +129,7 @@ $(document).ready(function () {
                     $(answerDiv).remove();
                     $("#correct-answer").append(rightDiv);
                     $(".rightAns").text("That's Correct!!");
+                    $("#image").html("<img src = ")
                     win++;
                 } else {
                     clearInterval(counter);
